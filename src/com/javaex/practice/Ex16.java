fd01539c688bf56174f4951de78f8ed698d697d3
jimin <jimin@218.38.137.28>
2024-06-21 12:54:56 +0900

    Revert "Revert "15~23""
    This reverts commit 617006644dff7984f66d6399682afc0ef6940cca.

--- src/com/javaex/practice/Ex16.java
+++ src/com/javaex/practice/Ex16.java
@@ -3,13 +3,17 @@ package com.javaex.practice;
 public class Ex16 {
 
 	public static void main(String[] args) {
-		/*
-		 * JVM에 대한 설명으로 틀린 것을 찾아 수정해보세요
-		 * 1. 운영체제별로 다른 jvm이 사용된다.
-		 * 2. JVM은 java.exe 명령어에 의해 구동된다
-         * 3. JVM은 바이트코드(~.class)를 기계어로 변환시키고 구동시키다.
-         * 4. 운영체제별로 동일한 JVM이 사용된다.
-         * 5. 바이트코드(~.class)는 운영체제에 독립적이만, JVM은 운영체제에 종속적이다.
-*/
+		
+		
+		int x = 1;
+		int y = 1;
+		
+		int a = ++x * 2;
+		int b = y-- * 2;
+		
+		System.out.println("a="+a); // a=4
+		System.out.println("b="+b); // b=2
+		System.out.println("x="+x); // x=2
+		System.out.println("y="+y); // y=0
 	}
 }
